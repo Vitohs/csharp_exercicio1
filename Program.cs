b9163d8e222456716901087c75bfa24053fc605b
vitohs <ninjadosani@gmail.com>
2024-05-10 11:31:49 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int intidade = 35; 
+Console.WriteLine(intidade);
+string strNome = "Maria";
+Console.WriteLine(strNome);
+double dblAltura = 3.45;
+Console.WriteLine(dblAltura);
+DateTime dtDate = new DateTime(1999, 04, 09) ;
+Console.WriteLine(dtDate);
+
+//constante
+const int intano = 12;
+Console.WriteLine(intano);
+
+//nullable number (permite nulo, só deixa o '?' na frente das variavel)
+double? dblNota = 7.80;
+Console.WriteLine(dblNota);
+
+//soma 
+
+int x = 77;
+int y = 66;
+
+Console.WriteLine(x + y);
+
